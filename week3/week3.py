294a5fce12983d6a85f532bcb61e626a968d91a7
yingyuchen <alan831007@gmail.com>
2025-10-27 08:10:32 +0800

    feat: Merge English names and addresses into Chinese data. Output one hotel information per line into hotels.csv .

---
+++ week3/week3.py
@@ -0,0 +1,42 @@
+import urllib.request as request
+import json
+import csv
+
+# parse data from URLs
+ch_url = "https://resources-wehelp-taiwan-b986132eca78c0b5eeb736fc03240c2ff8b7116.gitlab.io/hotels-ch"
+en_url = "https://resources-wehelp-taiwan-b986132eca78c0b5eeb736fc03240c2ff8b7116.gitlab.io/hotels-en"
+
+with request.urlopen(ch_url) as response:
+    ch_data = json.load(response)
+
+with request.urlopen(en_url) as response:
+    en_data = json.load(response)
+
+ch_hotel_list = ch_data["list"]
+en_hotel_list = en_data["list"]
+
+# Merge English names and addresses into Chinese data
+merged_hotel_list = []
+id_look_up = {hotel["_id"]: hotel for hotel in ch_hotel_list}
+for hotel in en_hotel_list:
+    id = hotel["_id"]
+    if id in id_look_up:
+        merged_hotel = {**id_look_up[id], **hotel}
+        merged_hotel_list.append(merged_hotel)
+
+# Output one hotel information per line into hotels.csv
+with open("hotels.csv", "w", encoding="utf-8", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(
+        ["旅宿名稱", "Hotel Name", "地址", "Address", "電話或手機號碼", "房間數"]
+    )
+    for hotel in merged_hotel_list:
+        # ChineseName,EnglishName,ChineseAddress,EnglishAddress,Phone,RoomCount
+        ch_name = hotel.get("旅宿名稱", "N/A")
+        en_name = hotel.get("hotel name", "N/A")
+        ch_address = hotel.get("地址", "N/A")
+        en_address = hotel.get("address", "N/A")
+        phone = hotel.get("電話或手機號碼", "N/A")
+        room_count = hotel.get("房間數", "N/A")
+
+        writer.writerow([ch_name, en_name, ch_address, en_address, phone, room_count])
