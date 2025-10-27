288eb39a0950cf6910ae1eb83f0ccf4cd142f089
yingyuchen <alan831007@gmail.com>
2025-10-27 17:48:18 +0800

    feat: Week 3 Task 4 - Implement "load more "

--- week3/week3.js
+++ week3/week3.js
@@ -2,57 +2,91 @@ const spotsURL = "https://cwpeng.github.io/test/assignment-3-1";
 const picturesURL = "https://cwpeng.github.io/test/assignment-3-2";
 const picHostURL = "https://www.travel.taipei";
 
+let spots = [];
+let pictures = [];
+
+let itemCount = 3;
+
+document.addEventListener("DOMContentLoaded", async () => {
+  setup();
+  try {
+    [spots, pictures] = await fetchData();
+    updateUI(spots, pictures);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+function setup() {
+  let loadMoreButton = document.getElementById("load-more-button");
+  loadMoreButton.addEventListener("click", () => {
+    loadMore(spots, pictures);
+  });
+}
+
 async function fetchData() {
   try {
     const spotsResponse = await fetch(spotsURL);
     const spotsData = await spotsResponse.json();
-    const spots = spotsData.rows;
+    spots = spotsData.rows;
     const picturesResponse = await fetch(picturesURL);
     const picturesData = await picturesResponse.json();
-    const pictures = picturesData.rows;
-
-    // spots for promotion display (we using flex box here)
-    const promotionSpots = spots.filter((spot) => {
-      return spot._id <= 3;
-    });
-
-    promotionSpots.forEach((spot, i) => {
-      let promotionDiv = document.querySelector(`.promotion${i + 1}`);
-      const image = createImageFrom(pictures, spot);
-      const textNode = document.createTextNode(spot.sname);
-      promotionDiv.appendChild(image);
-      promotionDiv.appendChild(textNode);
-    });
-
-    // spots for grid display
-    const grid_spots = spots.filter((spot) => {
-      return (4 <= spot._id) & (spot._id <= 13);
-    });
-    let gridDiv = document.querySelector(".content-grid");
-    grid_spots.forEach((spot, i) => {
-      let contentDiv = document.createElement("div");
-      contentDiv.classList.add("content");
-      let icon = document.createElement("i");
-      icon.classList.add("star-icon");
-      icon.textContent = "⭐";
-      let p = document.createElement("p");
-      p.textContent = spot.sname;
-      p.classList.add("content-footer");
-
-      const picturesURL = pictures.find(
-        (picture) => picture.serial === spot.serial
-      );
-      const firstPicURL = picturesURL.pics?.split(".jpg").at(0) + ".jpg";
-      contentDiv.style.backgroundImage = `url(${picHostURL + firstPicURL})`;
-      contentDiv.appendChild(icon);
-      contentDiv.appendChild(p);
-
-      gridDiv.appendChild(contentDiv);
-    });
-
+    pictures = picturesData.rows;
+    return [spots, pictures];
   } catch (error) {
     console.log(error);
   }
+}
+
+function updateUI(spots, pictures) {
+  // spots for promotion display (we using flex box here)
+  const promotionSpots = spots.filter((spot) => {
+    return spot._id <= 3;
+  });
+
+  promotionSpots.forEach((spot, i) => {
+    let promotionDiv = document.querySelector(`.promotion${i + 1}`);
+    const image = createImageFrom(pictures, spot);
+    const textNode = document.createTextNode(spot.sname);
+    promotionDiv.appendChild(image);
+    promotionDiv.appendChild(textNode);
+  });
+
+  // spots for grid display
+  loadMore(spots, pictures);
+}
+
+function loadMore(spots, pictures) {
+  // spots for grid display
+  let grid_spots = [];
+  let counter = 0;
+  for (let i = 0; i < 10; i++) {
+    if (i + itemCount < spots.length) {
+      grid_spots.push(spots[i + itemCount]);
+    }
+  }
+  let gridDiv = document.querySelector(".content-grid");
+  grid_spots.forEach((spot, i) => {
+    let contentDiv = document.createElement("div");
+    contentDiv.classList.add("content");
+    let icon = document.createElement("i");
+    icon.classList.add("star-icon");
+    icon.textContent = "⭐";
+    let p = document.createElement("p");
+    p.textContent = spot.sname;
+    p.classList.add("content-footer");
+
+    const picturesURL = pictures.find(
+      (picture) => picture.serial === spot.serial
+    );
+    const firstPicURL = picturesURL.pics?.split(".jpg").at(0) + ".jpg";
+    contentDiv.style.backgroundImage = `url(${picHostURL + firstPicURL})`;
+    contentDiv.appendChild(icon);
+    contentDiv.appendChild(p);
+
+    gridDiv.appendChild(contentDiv);
+  });
+  itemCount += 10;
 }
 
 function createImageFrom(pictures, spot) {
@@ -65,7 +99,3 @@ function createImageFrom(pictures, spot) {
   image.alt = spot.sname;
   return image;
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetchData();
-});
