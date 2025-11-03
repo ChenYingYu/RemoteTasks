d6dcd14f6a225471a39da22be1a4355bd24dc694
yingyuchen <alan831007@gmail.com>
2025-11-03 10:46:32 +0800

    feat: Week 4 Task 3 - Implement session management for login and logout functionality

--- week4/main.py
+++ week4/main.py
@@ -4,8 +4,16 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+# Task 3
+from starlette.middleware.sessions import SessionMiddleware
+import secrets
+
+SESSION_KEY = "LOGGED-IN"
+secret_key = secrets.token_urlsafe(32)
+
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=secret_key)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
@@ -23,13 +31,16 @@ from fastapi.responses import RedirectResponse
 
 
 @app.post("/login/")
-async def login(email: Annotated[str, Form()], password: Annotated[str, Form()]):
+async def login(
+    request: Request, email: Annotated[str, Form()], password: Annotated[str, Form()]
+):
     if not email or not password:
         return RedirectResponse(
             url="/ohoh?msg=請輸入信箱和密碼", status_code=status.HTTP_303_SEE_OTHER
         )
 
     if email == "abc@abc.com" and password == "abc":
+        request.session[SESSION_KEY] = True
         return RedirectResponse(url="/member", status_code=status.HTTP_303_SEE_OTHER)
 
     else:
@@ -40,9 +51,18 @@ async def login(email: Annotated[str, Form()], password: Annotated[str, Form()])
 
 @app.get("/member", response_class=HTMLResponse)
 async def member_page(request: Request):
+    if SESSION_KEY not in request.session or not request.session[SESSION_KEY]:
+        return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
     return templates.TemplateResponse("member.html", {"request": request})
 
 
 @app.get("/ohoh", response_class=HTMLResponse)
 async def error_page(request: Request, msg: str):
     return templates.TemplateResponse("ohoh.html", {"request": request, "msg": msg})
+
+
+# Task 3
+@app.get("/logout", response_class=HTMLResponse)
+async def logout(request: Request):
+    request.session[SESSION_KEY] = False
+    return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
