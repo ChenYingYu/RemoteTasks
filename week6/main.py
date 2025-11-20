f12f2e03961ec2d9d67d2f9d39106c1a250eb3b7
yingyuchen <alan831007@gmail.com>
2025-11-20 21:07:26 +0800

    feat: Week 6 Task 2 -Implement signup functionality with form validation and database integration

--- week6/main.py
+++ week6/main.py
@@ -1,7 +1,23 @@
+# Task 1
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+# Task 2
+from typing import Annotated
+from fastapi import Form, status
+from dotenv import load_dotenv
+from fastapi.responses import RedirectResponse
+import os
+import mysql.connector
+
+load_dotenv()
+
+
+mysql_user = os.getenv("MYSQL_USER")
+mysql_password = os.getenv("MYSQL_PASSWORD")
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -9,9 +25,42 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+def get_website_db_connection():
+    return mysql.connector.connect(
+        user=mysql_user, password=mysql_password, host="localhost", database="website"
+    )
+
+
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/signup")
+async def signup(
+    request: Request,
+    signup_name: Annotated[str, Form()],
+    signup_email: Annotated[str, Form()],
+    signup_password: Annotated[str, Form()],
+):
+    db = get_website_db_connection()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM member WHERE email=%s;", (signup_email,))
+    email_exists = cursor.fetchone()
+
+    if email_exists:
+        db.close()
+        return RedirectResponse(
+            url="/ohoh?msg=重複的電子郵件", status_code=status.HTTP_303_SEE_OTHER
+        )
+
+    cursor.execute(
+        "INSERT INTO member (name, email, password) VALUES (%s, %s, %s);",
+        (signup_name, signup_email, signup_password),
+    )
+    db.commit()
+    db.close()
+    return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
 
 
 @app.get("/member", response_class=HTMLResponse)
@@ -20,7 +69,5 @@ async def member_page(request: Request):
 
 
 @app.get("/ohoh", response_class=HTMLResponse)
-async def error_page(request: Request):
-    return templates.TemplateResponse(
-        "ohoh.html", {"request": request, "msg": "帳號、或密碼輸入錯誤"}
-    )
+async def error_page(request: Request, msg: str):
+    return templates.TemplateResponse("ohoh.html", {"request": request, "msg": msg})
