5bb166afe2737806ec694536af57df86e4d112e0
yingyuchen <alan831007@gmail.com>
2025-11-24 06:49:47 +0800

    feat: Week 7 Task 2 - Implement member search functionality on member page

--- week7/static/member.js
+++ week7/static/member.js
@@ -12,3 +12,21 @@ document.querySelectorAll(".delete-button").forEach((button) => {
     }
   });
 });
+
+document.getElementById("search-button").addEventListener("click", async () => {
+  const memberId = document.getElementById("search-id").value.trim();
+  if (!isNaN(memberId)) {
+    const response = await fetch(`/api/member/${memberId}`);
+    if (response.ok) {
+      const data = await response.json();
+      const resultDiv = document.getElementById("search-result");
+      if (data.data) {
+        resultDiv.innerHTML = `
+          <p>${data.data.name}(${data.data.email})</p>
+        `;
+      } else {
+        resultDiv.innerHTML = `<p>無此會員</p>`;
+      }
+    }
+  }
+});
