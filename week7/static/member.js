b9116caa76de6438f77515210415a166b0753c75
yingyuchen <alan831007@gmail.com>
2025-11-24 18:31:11 +0800

    chore: Remove unused code snippet

--- week7/static/member.js
+++ week7/static/member.js
@@ -1,18 +1,3 @@
-document.querySelectorAll(".delete-button").forEach((button) => {
-  button.addEventListener("click", () => {
-    const messageId = button.getAttribute("message-id");
-    if (confirm("確定要刪除嗎？")) {
-      fetch(`/deleteMessage/${messageId}`, {
-        method: "DELETE",
-      }).then((response) => {
-        if (response.ok) {
-          window.location.href = "/member";
-        }
-      });
-    }
-  });
-});
-
 document.getElementById("search-button").addEventListener("click", async () => {
   const memberId = document.getElementById("search-id").value.trim();
   if (!isNaN(memberId)) {
